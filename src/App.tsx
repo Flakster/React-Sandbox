1cf4e5ad0bc34c1b164f9ca6888f221bc7070849
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-06 16:55:15 -0500

    add the files for the listgroup example

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ListGroup from './components/ListGroup'
+
+function App() {
+  let items=['Bogota','Pereira', 'Caparrapi', 'La Dorada'];
+  const handleSelectItem=(item:string)=>{
+    console.log(item)
+  }
+  return (
+    <>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+    </>
+  )
+}
+
+export default App
