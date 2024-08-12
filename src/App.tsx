b21c8be3de03d3946661e87940eb2141c83cbede
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-12 08:41:32 -0500

    style the code with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import './App.css'
-import ListGroup from './components/ListGroup'
+import "./App.css";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items=['Bogota','Pereira', 'Caparrapi', 'La Dorada'];
-  const handleSelectItem=(item:string)=>{
-    console.log(item)
-  }
+  let items = ["Bogota", "Pereira", "Caparrapi", "La Dorada"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
