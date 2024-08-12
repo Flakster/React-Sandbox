95fde55008b6adb8fd30d6a7918101080e167d51
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-12 08:59:28 -0500

    set the project for the cssModules exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import "./App.css";
-import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["Bogota", "Pereira", "Caparrapi", "La Dorada"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
     </>
   );
 }
