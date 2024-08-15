14e426ef63c3db2af019290027ff34d29a70e3ed
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-15 14:12:50 -0500

    add functionality onClick, state manageament,  and styled components

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,41 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+interface ListItemProps {
+  active: boolean;
+}
+
+const ListItem = styled.li<ListItemProps>`
+  border: 1px solid black;
+  background-color: ${(props) => (props.active ? "mistyRose" : "none")};
+  padding: 5px 0;
+`;
 interface Props {
-    items: string[];
+  items: string[];
 }
 
-const ListGroup = ({items}:Props) => {
+const ListGroup = ({ items }: Props) => {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
-    <ul>
-        {items.map((item,index)=> (
-            <li key={index}>{item}</li>
-        ))}
-    </ul>
-  )
-}
+    <List>
+      {items.map((item, index) => (
+        <ListItem
+          active={index === selectedIndex}
+          key={item}
+          onClick={() => {
+            setSelectedIndex(index);
+          }}
+        >
+          {item}
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
-export default ListGroup
+export default ListGroup;
